6fc0ebf29dbf5e2c7300182415bf5ae4e576d68d
Angel Aranguiz <angel.aranguiz@gmail.com>
2025-03-31 16:51:00 +0000

    Created Suppliers main view, filters on table, global filter

---
+++ webapp/controller/Suppliers.controller.js
@@ -0,0 +1,48 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+], (Controller,JSONModel,Filter,FilterOperator) => {
+    "use strict";
+
+    return Controller.extend("com.agarangu.supplierssapfiori.controller.Suppliers", {
+        onInit() {
+            const oView = this.getView();
+            oView.setModel(new JSONModel({
+				globalFilter: ""
+			}), "ui");
+			this._oGlobalFilter = null;
+        },
+        _filter: function() {
+			let oFilter = null;
+			if (this._oGlobalFilter) {
+				oFilter = this._oGlobalFilter;
+			}
+			this.byId("table").getBinding().filter(oFilter);
+		},
+		filterGlobally: function(oEvent) {
+			const sQuery = String(oEvent.getParameter("query"));
+			this._oGlobalFilter = null;
+			if (sQuery) {
+				this._oGlobalFilter = new Filter([
+					new Filter("CompanyName", FilterOperator.Contains, sQuery),
+					new Filter("Country", FilterOperator.Contains, sQuery),
+				], false);
+			}
+			this._filter();
+		},
+        clearAllFilters: function(oEvent) {
+			const oTable = this.byId("table");
+			const oUiModel = this.getView().getModel("ui");
+			oUiModel.setProperty("/globalFilter", "");
+			this._oGlobalFilter = null;
+			this._filter();
+			const aColumns = oTable.getColumns();
+			for (let i = 0; i < aColumns.length; i++) {
+				oTable.filter(aColumns[i], null);
+			}
+		},
+        
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/agarangu/supplierssapfiori/test/unit/controller/Suppliers.controller"
+], function () {
+	"use strict";
+});
